c71f416e623d01150bb5cb6d54d42526dbdd2f73
pedro henrique de souza <pedromotoehm@gmail.com>
2022-03-08 18:21:25 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-
 import React from "react";
-import Main from './components/main'
-import './App.css';
-export default function App() {
-  return <Main/>
+import './App.css'
+export default function App () { 
+  return <h1>Hello world!</h1>
 }
-
 
