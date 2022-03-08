b927a9e2966ab43b18401e7c9ee1ed30c4d21660
pedro henrique de souza <pedromotoehm@gmail.com>
2022-03-08 13:04:13 -0300

    atualizaçao aula 1
    aula 1 completa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello word!
-        </p>
-        <p>lorem</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import './App.css'
+export default function App () { 
+  return <h1>Hello world!</h1>
 }
 
-export default App;
