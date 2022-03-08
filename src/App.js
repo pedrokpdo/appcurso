0ec3c8e81cfc59d8db1d7eb9e33a89021cf67e7d
pedro henrique de souza <pedromotoehm@gmail.com>
2022-03-08 14:23:19 -0300

    nome comit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
-export default function App () {
-  return  (<>
-  <h1>Olá mundo</h1>
-  </>)
-};
+
+import React from "react";
+import './App.css'
+export default function App() {
+  return <h1>Hello world!</h1>
+}
+
 
